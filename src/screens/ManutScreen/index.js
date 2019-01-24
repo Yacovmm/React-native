a3b1f93f493de5774a8584541aaec3d908bf7b17
Aula Radix <radix@SJ208F13.local>
2019-01-24 12:14:25 -0200

    Criaçao tela inclusao

---
+++ src/screens/ManutScreen/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet,FlatList, View,Text,SafeAreaView,ActivityIndicator,TextInput,Button } from 'react-native';
+
+class ManutScreen extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+             titulo = "",
+             descricao= ""  
+        }
+    }
+ 
+    render() {
+        return (
+
+          <View>
+            <Text>Titulo</Text>
+            <TextInput value={this.state.titulo}  placeholder="Digite o titulo aqui"></TextInput>
+            <Text>Descrição</Text>
+            
+            <TextInput
+                value={this.state.descricao}
+                style={styles.textArea}
+                
+                placeholder="Descrição"
+                placeholderTextColor="grey"
+                numberOfLines={10}
+                multiline={true}
+                />
+            
+                <Button title="Adicionar" onPress={ () => props.action.addItem(this.state) } />
+          </View>
+        );
+    }
+
+}
+const styles = StyleSheet.create({
+    textAreaContainer: {
+      borderColor: COLORS.grey20,
+      borderWidth: 1,
+      padding: 5
+    },
+    textArea: {
+      height: 150,
+      justifyContent: "flex-start"
+    }
+  })
